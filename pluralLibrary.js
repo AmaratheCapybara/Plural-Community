e09ea458b635f8b1b1cc662c774594e9f6079cf9
AmaratheCapybara <AmaranthAragon@gmail.com>
2024-09-23 00:31:38 -0400

    Create pluralLibrary.js
    Used for the plural community to help with terms commonly used when discussing systems.

---
+++ pluralLibrary.js
@@ -0,0 +1,68 @@
+// Path: pluralLibrary.js
+
+/**
+ * A JavaScript library that contains variables for common terms used by the plural community.
+ */
+
+const PluralSystemTerms = {
+
+    // System Types
+    systemTypes: {
+        MEDIAN: "Median System",
+        POLYFRAGMENTED: "Polyfragmented System",
+        ENDOGENIC: "Endogenic System",
+        TRAUMAGENIC: "Traumagenic System",
+        MIXED_ORIGINS: "Mixed Origins System",
+        SYSTEM: "System",
+        SINGLET: "Singlet",
+        COLLECTIVE: "Collective",
+    },
+
+    // Roles within the system
+    roles: {
+        HOST: "Host",
+        PROTECTOR: "Protector",
+        GATEKEEPER: "Gatekeeper",
+        INTERNAL_SELF_HELPER: "Internal Self Helper",
+        PERSECUTOR: "Persecutor",
+        CARETAKER: "Caretaker",
+        CORE: "Core",
+        LEADER: "Leader",
+    },
+
+    // Activities
+    activities: {
+        CO_CONSCIOUS: "Co-consciousness",
+        FRONTING: "Fronting",
+        SWITCHING: "Switching",
+    },
+
+    // Identity Terms
+    identityTerms: {
+        HEADMATE: "Headmate",
+        ALTER: "Alter",
+        PART: "Part",
+        MEMBER: "System Member",
+        FRONTER: "Fronter",
+        COFROTNER: "Co-Fronter",
+        LITTLE: "Little",
+        INTROJECT: "Introject",
+        FICTIVE: "Fictive",
+    },
+
+    // States
+    states: {
+        DISSOCIATION: "Dissociation",
+        AMNESIA_BARRIER: "Amnesia Barrier",
+        FLASHBACK: "Flashback",
+        SWITCHING_EPISODE: "Switching Episode",
+        BLURRY: "Blurry",  // Newly added state
+        FRONTING: "Fronting",
+    },
+    // descriptors for the internal world and alter experiences
+    alterExperiences:{
+        MIND_SPACE: "Mind Space",
+        IMAGINARY_WORLD: "Imaginary World",
+        EXOMEMORIES: "Exomemories"
+    }
+};
